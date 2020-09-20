b8e58faa8dfaedcf687169ba684d34f2d05a649d
prasad hewage <hewage18@gmail.com>
2020-09-20 07:07:37 +0530

    init commit

---
+++ build/precache-manifest.ccbec016fbf5f11dec55c4cfdfa3f770.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146155b690168548d07f933f8ec89707",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fec6222a0b67059c6209",
+    "url": "/static/css/main.cc927401.chunk.css"
+  },
+  {
+    "revision": "8c2019585dce163e07da",
+    "url": "/static/js/2.a92c1ddd.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.a92c1ddd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fec6222a0b67059c6209",
+    "url": "/static/js/main.8b21e19a.chunk.js"
+  },
+  {
+    "revision": "ea630fe81389fbceefbb",
+    "url": "/static/js/runtime-main.bf9f4589.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "268676d96d6f9c2fbaec2e64cbd49235",
+    "url": "/static/media/esLogo.268676d9.jpg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "3a97572ef873306c9c78f4eba8e622f6",
+    "url": "/static/media/pic1.3a97572e.jpg"
+  },
+  {
+    "revision": "2cf18fa8aeb2528aea1f503c2de8b031",
+    "url": "/static/media/pic10.2cf18fa8.jpg"
+  },
+  {
+    "revision": "622913fde38df7e8becd987ee3a5ab20",
+    "url": "/static/media/pic11.622913fd.jpg"
+  },
+  {
+    "revision": "a84423a319e2d3f5ec6e10e3f544eb34",
+    "url": "/static/media/pic12.a84423a3.jpg"
+  },
+  {
+    "revision": "6b9349c547bfd317dddeb3f1c3f93b4e",
+    "url": "/static/media/pic13.6b9349c5.jpg"
+  },
+  {
+    "revision": "406960739298fe642bf526be1ff05a2d",
+    "url": "/static/media/pic2.40696073.jpg"
+  },
+  {
+    "revision": "fceb5274305a960f78d53724970c4f02",
+    "url": "/static/media/pic3.fceb5274.jpg"
+  },
+  {
+    "revision": "85a3a8ade543a5eab5b04a75d54b7ff1",
+    "url": "/static/media/pic4.85a3a8ad.jpg"
+  },
+  {
+    "revision": "50ab2f85109e63545991b07f1f44b068",
+    "url": "/static/media/pic5.50ab2f85.jpg"
+  },
+  {
+    "revision": "0068acb7d52d69885d02df3576796ad0",
+    "url": "/static/media/pic6.0068acb7.jpg"
+  },
+  {
+    "revision": "952976fe72021045fa0cdb10ca5c0fa7",
+    "url": "/static/media/pic7.952976fe.jpg"
+  },
+  {
+    "revision": "dc4a86cce7eefa590c5e4aa9623275a7",
+    "url": "/static/media/pic8.dc4a86cc.jpg"
+  },
+  {
+    "revision": "40c6ca3439c8a271560b8c90cf65cd2b",
+    "url": "/static/media/pic9.40c6ca34.jpg"
+  }
+]);
---
+++ src/components/sections/FeaturesTiles.js
@@ -0,0 +1,261 @@
+import React from 'react';
+import classNames from 'classnames';
+import { SectionTilesProps } from '../../utils/SectionProps';
+import SectionHeader from './partials/SectionHeader';
+import Image from '../elements/Image';
+
+const propTypes = {
+  ...SectionTilesProps.types
+}
+
+const defaultProps = {
+  ...SectionTilesProps.defaults
+}
+const FeaturesTiles = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  pushLeft,
+  ...props
+}) => {
+
+  const outerClasses = classNames(
+    'features-tiles section',
+    topOuterDivider && 'has-top-divider',
+    bottomOuterDivider && 'has-bottom-divider',
+    hasBgColor && 'has-bg-color',
+    invertColor && 'invert-color',
+    className
+  );
+
+  const innerClasses = classNames(
+    'features-tiles-inner section-inner pt-0',
+    topDivider && 'has-top-divider',
+    bottomDivider && 'has-bottom-divider'
+  );
+
+  const tilesClasses = classNames(
+    'tiles-wrap center-content',
+    pushLeft && 'push-left'
+  );
+
+  const sectionHeader = {
+    title: 'Honda Civic (seventh generation)',
+    paragraph: 'The seventh generation Honda Civic is an automobile which was produced by Honda from 2000 to 2005. It was released in September 2000 as a 2001 model.'
+  };
+
+  return (
+    <section
+      {...props}
+      className={outerClasses}
+    >
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={tilesClasses}>
+
+            <div className="tiles-item reveal-from-bottom">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/cars/pic1.jpg')}
+                      alt="Features tile icon 01"
+                      width={350}
+                      height={350} />
+                  </div>
+                </div>
+                {/* <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                    Owner
+                    </h4>
+                  <p className="m-0 text-sm">
+                    Manoj
+                    </p>
+                </div> */}
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/cars/pic2.jpg')}
+                      alt="Features tile icon 01"
+                      width={350}
+                      height={350} />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/cars/pic3.jpg')}
+                      alt="Features tile icon 01"
+                      width={350}
+                      height={350} />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/cars/pic4.jpg')}
+                      alt="Features tile icon 01"
+                      width={350}
+                      height={350} />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/cars/pic5.jpg')}
+                      alt="Features tile icon 01"
+                      width={350}
+                      height={350} />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/cars/pic6.jpg')}
+                      alt="Features tile icon 01"
+                      width={350}
+                      height={350} />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/cars/pic7.jpg')}
+                      alt="Features tile icon 01"
+                      width={350}
+                      height={350} />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/cars/pic8.jpg')}
+                      alt="Features tile icon 01"
+                      width={350}
+                      height={350} />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/cars/pic9.jpg')}
+                      alt="Features tile icon 01"
+                      width={350}
+                      height={350} />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/cars/pic10.jpg')}
+                      alt="Features tile icon 01"
+                      width={350}
+                      height={350} />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/cars/pic11.jpg')}
+                      alt="Features tile icon 01"
+                      width={350}
+                      height={350} />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/cars/pic12.jpg')}
+                      alt="Features tile icon 01"
+                      width={350}
+                      height={350} />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/cars/pic13.jpg')}
+                      alt="Features tile icon 01"
+                      width={350}
+                      height={350} />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+FeaturesTiles.propTypes = propTypes;
+FeaturesTiles.defaultProps = defaultProps;
+
+export default FeaturesTiles;
